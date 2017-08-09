038aa1c34d1f78d172d4be792c39b9032a141ef3
HanMJ <915660592@qq.com>
2017-08-09 13:15:53 +0800

    Initial Commit

---
+++ WechatInputTextView/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  WechatInputTextView
+//
+//  Created by MJ on 2017/8/9.
+//  Copyright © 2017年 韩明静. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ WechatInputTextView/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  WechatInputTextView
+//
+//  Created by MJ on 2017/8/9.
+//  Copyright © 2017年 韩明静. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
